92b00ce768ab826be7c03a2b15b795bd508616e8
Shawkot <62845541+Shawkot@users.noreply.github.com>
2020-03-30 22:59:34 +0600

    Create for_loop1.c

---
+++ for_loop1.c
@@ -0,0 +1,13 @@
+// Prints a row of 4 question marks with a loop
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        printf("?");
+    }
+    printf("\n");
+    
+}
