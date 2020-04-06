896c9f20e6f18ba89cfcdbeab95d4fcf993374e3
Shawkot <62845541+Shawkot@users.noreply.github.com>
2020-04-07 01:11:40 +0600

    Create char1.c

---
+++ char1.c
@@ -0,0 +1,10 @@
+// create an array of character //
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char *name[4] = {"shawkot", "srabu", "riaz", "rahnuma"};
+    printf("%s \n", name[3]);
+    return 0;
+}
