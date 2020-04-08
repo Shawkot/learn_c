3e70c22ab846dedb2d34421c0fa17ab0b3897970
Shawkot <62845541+Shawkot@users.noreply.github.com>
2020-04-08 13:18:41 +0600

    Update agree.c

--- agree.c
+++ agree.c
@@ -1,16 +1,21 @@
-//This program will say if one is agree or not//
-#include <cs50.h>
+//This program will say if one is agree or not
+// by using getchar(); function
+
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    char c=get_char("Do you agreed?\n");
-    if(c=='y' || c=='Y')
+  char cond1;
+  printf("enter your keyword:");
+  cond1 = getchar();
+  if(cond1 =='y')
+  {
+      printf("agreed");
+  }
+  else
     {
-        printf("Agreed\n");
+    printf("not agreed");
     }
-    else if (c=='n' || c=='N')
-    {
-        printf("NOt agreed\n");
-    }
+    return 0;
 }
