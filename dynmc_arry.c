e02ab2566bd667877d5f2f46182dde4c1ea4cc23
Shawkot <62845541+Shawkot@users.noreply.github.com>
2020-04-09 13:34:32 +0600

    Create dynmc_arry.c

---
+++ dynmc_arry.c
@@ -0,0 +1,22 @@
+// This program will create a dynamic array
+// by using calloc() function
+
+#include <stdio.h>
+    int main()
+    {
+        int * arr_dynamic = NULL;
+        int elements = 4, i;
+        arr_dynamic = calloc(elements, sizeof(int)); //Array with 2 integer blocks
+        for (i = 0; i < elements; i++)
+        {
+         arr_dynamic[i] = i;
+         printf("arr_dynamic[%d]=%d\n", i, arr_dynamic[i]);
+        }
+        elements = 8;
+        arr_dynamic = realloc(arr_dynamic, elements * sizeof(int)); //reallocate 4 elements
+        printf("After realloc\n");
+        for (i = 2; i < elements; i++) arr_dynamic[i] = i;
+        for (i = 0; i < elements; i++) printf("arr_dynamic[%d]=%d\n", i, arr_dynamic[i]);
+        free(arr_dynamic);
+        return 0;
+    }
