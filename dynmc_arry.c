517c5b9f7d29fcc7647efb4c1e6a042bdb1ef75b
Shawkot <62845541+Shawkot@users.noreply.github.com>
2020-04-09 13:38:55 +0600

    Update dynmc_arry.c

--- dynmc_arry.c
+++ dynmc_arry.c
@@ -6,14 +6,14 @@
     {
         int * arr_dynamic = NULL;
         int elements = 4, i;
-        arr_dynamic = calloc(elements, sizeof(int)); //Array with 2 integer blocks
+        arr_dynamic = calloc(elements, sizeof(int)); //Array with 4 integer blocks
         for (i = 0; i < elements; i++)
         {
          arr_dynamic[i] = i;
          printf("arr_dynamic[%d]=%d\n", i, arr_dynamic[i]);
         }
         elements = 8;
-        arr_dynamic = realloc(arr_dynamic, elements * sizeof(int)); //reallocate 4 elements
+        arr_dynamic = realloc(arr_dynamic, elements * sizeof(int)); //reallocate 8 elements
         printf("After realloc\n");
         for (i = 2; i < elements; i++) arr_dynamic[i] = i;
         for (i = 0; i < elements; i++) printf("arr_dynamic[%d]=%d\n", i, arr_dynamic[i]);
